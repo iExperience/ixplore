5991fcd732ef32ec5fbfced9e10a200ccc708401
Brian Ge <briange1997@gmail.com>
2016-07-06 22:37:34 +0200

    Submitted to iTunes connect

---
+++ iXplore/CustomCalendarViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CustomCalendarViewController.swift
+//  iXplore
+//
+//  Created by Brian Ge on 7/6/16.
+//  Copyright © 2016 iXperience. All rights reserved.
+//
+
+import UIKit
+
+class CustomCalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
