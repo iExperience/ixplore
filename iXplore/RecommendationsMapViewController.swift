dbfe4dadba07452d8f740e585b0a7467c7c9f342
alexge1 <alexanderge@college.harvard.edu>
2016-07-12 12:33:57 +0200

    ixplore map up in menu

---
+++ iXplore/RecommendationsMapViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RecommendationsMapViewController.swift
+//  iXplore
+//
+//  Created by Alexander Ge on 7/12/16.
+//  Copyright © 2016 iXperience. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class RecommendationsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // Google's setup code
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        // End Google's setup code
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
